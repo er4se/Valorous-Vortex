7c75c65aef73dc5fd94dc6f5fa302ba4db01ad10
erase <al.annko@mail.ru>
2023-12-21 20:36:28 +0300

    2 часа потной работы без результата по коду, но с результатом по знаниям!!!

---
+++ VV/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VV.Models
+{
+    class User
+    {
+        private string id;
+        private string login;
+        private string password;
+
+        public string NickName { get; set; }
+    }
+}
